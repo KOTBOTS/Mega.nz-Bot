d76495fe04a8e077c6ec9b4c1bcfab9554ca78c2
KOTBOTS <89208197+KOTBOTS@users.noreply.github.com>
2021-10-08 18:57:18 +0530

    Update mega_dl.py

--- megadl/mega_dl.py
+++ megadl/mega_dl.py
@@ -103,7 +103,7 @@ async def megadl(_, message: Message):
         if not guessedfilemime.mime:
             await download_msg.edit("**Trying to Upload Now!** \n\n**Error:** `Can't Get File Mime Type! Sending as a Document!`")
             await message.reply_document(magapylol, progress=progress_for_pyrogram, progress_args=("**Trying to Upload Now!** \n", download_msg, start_time))
-            await download_msg.edit(f"**Successfully Uploaded** \n\n**Join @NexaBotsUpdates If You're Enjoying This Bot**")
+            await download_msg.edit(f"**Successfully Uploaded** \n\n**Join @KOT_BOTS If You're Enjoying This Bot**")
             shutil.rmtree(basedir + "/" + userpath)
             return
         filemimespotted = guessedfilemime.mime
@@ -111,14 +111,14 @@ async def megadl(_, message: Message):
         if "image/gif" in filemimespotted:
             await download_msg.edit("**Trying to Upload Now!**")
             await message.reply_animation(magapylol, progress=progress_for_pyrogram, progress_args=("**Trying to Upload Now!** \n", download_msg, start_time))
-            await download_msg.edit(f"**Successfully Uploaded** \n\n**Join @NexaBotsUpdates If You're Enjoying This Bot**")
+            await download_msg.edit(f"**Successfully Uploaded** \n\n**Join @KOT_BOTS If You're Enjoying This Bot**")
             shutil.rmtree(basedir + "/" + userpath)
             return
         # Checking if it's a image
         if "image" in filemimespotted:
             await download_msg.edit("**Trying to Upload Now!**")
             await message.reply_photo(magapylol, progress=progress_for_pyrogram, progress_args=("**Trying to Upload Now!** \n", download_msg, start_time))
-            await download_msg.edit(f"**Successfully Uploaded** \n\n**Join @NexaBotsUpdates If You're Enjoying This Bot**")
+            await download_msg.edit(f"**Successfully Uploaded** \n\n**Join @KOT_BOTS If You're Enjoying This Bot**")
         # Checking if it's a video
         elif "video" in filemimespotted:
             await download_msg.edit("**Generating Data...**")
@@ -127,17 +127,17 @@ async def megadl(_, message: Message):
             thumbnail_path = f"{alreadylol}/thumbnail.jpg"
             subprocess.call(['ffmpeg', '-i', magapylol, '-ss', '00:00:00.000', '-vframes', '1', thumbnail_path])
             await message.reply_video(magapylol, duration=vidduration, thumb=thumbnail_path, progress=progress_for_pyrogram, progress_args=("**Trying to Upload Now!** \n", download_msg, start_time))
-            await download_msg.edit(f"**Successfully Uploaded** \n\n**Join @NexaBotsUpdates If You're Enjoying This Bot**")
+            await download_msg.edit(f"**Successfully Uploaded** \n\n**Join @KOT_BOTS If You're Enjoying This Bot**")
         # Checking if it's a audio
         elif "audio" in filemimespotted:
             await download_msg.edit("**Trying to Upload Now!**")
             await message.reply_audio(magapylol, progress=progress_for_pyrogram, progress_args=("**Trying to Upload Now!** \n", download_msg, start_time))
-            await download_msg.edit(f"**Successfully Uploaded** \n\n**Join @NexaBotsUpdates If You're Enjoying This Bot**")
+            await download_msg.edit(f"**Successfully Uploaded** \n\n**Join @KOT_BOTS If You're Enjoying This Bot**")
         # If it's not a image/video or audio it'll reply it as doc
         else:
             await download_msg.edit("**Trying to Upload Now!**")
             await message.reply_document(magapylol, progress=progress_for_pyrogram, progress_args=("**Trying to Upload Now!** \n", download_msg, start_time))
-            await download_msg.edit(f"**Successfully Uploaded** \n\n**Join @NexaBotsUpdates If You're Enjoying This Bot**")
+            await download_msg.edit(f"**Successfully Uploaded** \n\n**Join @KOT_BOTS If You're Enjoying This Bot**")
     try:
         shutil.rmtree(basedir + "/" + userpath)
         print("Successfully Removed Downloaded File and the folder!")
